b389c289483f785eb09b68e294a0ba7441fdd3c6
Heisenburger69 <apkforgss@gmail.com>
2019-11-18 18:44:43 +0530

    Basic Mechanism

--- src/Heisenburger69/TempRanks/Main.php
+++ src/Heisenburger69/TempRanks/Main.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Heisenburger69\TempRanks;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
 
 class Main extends PluginBase implements Listener {
 
@@ -19,23 +21,184 @@ class Main extends PluginBase implements Listener {
         @mkdir($this->getDataFolder());
     }
 
-	public function onEnable() : void{
-        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, ["Check Rank on Join" => "true", "Check Rank during Session" => "false"]);
+    public function onEnable() : void{
+        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, ["Time Left Message" => "You have {time_left} on your temporary {temprank} rank", "Rank Expired Message" => "Your {temprank} Rank has expired"]);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->db = new \SQLite3($this->getDataFolder() . "Ranks.db");
+        $this->db->exec("CREATE TABLE IF NOT EXISTS ranks (player TEXT PRIMARY KEY COLLATE NOCASE, oldrank TEXT, endtime TEXT);");
+    }
 
-	}
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $playername = $player->getName();
+        $time = $this->getTimeLeft($playername);
+        $pp = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+        $rank =  $pp->getUserDataMgr()->getGroup($pp->getPlayer($playername), );
+        if($time !== null) {
+            $msg = $this->config->get("Time Left Message");
+            $msg = str_replace(array("{time_left}", "{temprank}", "&"), array($time, $rank, "ยง"), $msg);
+            $player->sendMessage($msg);
+        }
+        $exp = $this->getExpiryDate($playername);
+        if($exp === null) {
+            return;
+        }
+        if(strtotime($exp) < time()) {
+            $msg = $this->config->get("Rank Expired Message");
+            $msg = str_replace(array("{temprank}", "&"), array($rank, "ยง"), $msg);
+            $player->sendMessage($msg);
+            $this->removeRank($playername);
+        }
+    }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         switch($command->getName()){
             case "temprank":
-                $this->getServer()->getPluginManager()->registerEvents($this, $this);
-                $this->db = new \SQLite3($this->getDataFolder() . "Ranks.db");
-                $this->db->exec("CREATE TABLE IF NOT EXISTS ranks (player TEXT PRIMARY KEY COLLATE NOCASE, temprank TEXT, oldrank TEXT, time TEXT);");
+                $pp = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+                if($sender->hasPermission("temprank.command")) {
+                    if(isset($args[0])) {
+                        if($args[0] === "set") {
+                            if(isset($args[1])) {
+                                $oldrank =  $pp->getUserDataMgr()->getGroup($pp->getPlayer($args[1]), );
+                                $player = $this->getServer()->getPlayer($args[0]);
+                                if($player !== null) {
+                                    $playername = $player->getName();
+                                } else {
+                                    $playername = (string)$args[1];
+                                }
+                                if(isset($args[2])) {
+                                    $group = $pp->getGroup($args[2]);
+                                    if($group !== null) {
+                                        if(isset($args[3])) {
+                                            $num = (int)$args[3];
+                                            if(is_int($num) && $num > 0) {
+                                                $length = $num;
+                                                if(isset($args[4])) {
+                                                    switch ($args[4]) {
+                                                        case "minutes":
+                                                            $time = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s")." +$length minutes"));
+                                                            $stmt = $this->db->prepare("INSERT OR REPLACE INTO ranks (player, oldrank, endtime) VALUES (:player, :oldrank, :endtime);");
+                                                            $stmt->bindValue(":player", $playername);
+                                                            $stmt->bindValue(":oldrank", $oldrank);
+                                                            $stmt->bindValue(":endtime", $time);
+                                                            $stmt->execute();
+                                                            $ppplayer = $pp->getPlayer($args[1]);
+                                                            $pp->setGroup($ppplayer, $group);
+                                                            $sender->sendMessage(C::GREEN . "Rank set ree");
+                                                            break;
+                                                        case "hours":
+                                                            $time = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s")." +$length hours"));
+                                                            $stmt = $this->db->prepare("INSERT OR REPLACE INTO ranks (player, oldrank, endtime) VALUES (:player, :oldrank, :endtime);");
+                                                            $stmt->bindValue(":player", $playername);
+                                                            $stmt->bindValue(":oldrank", $oldrank);
+                                                            $stmt->bindValue(":endtime", $time);
+                                                            $stmt->execute();
+                                                            $ppplayer = $pp->getPlayer($args[1]);
+                                                            $pp->setGroup($ppplayer, $group);
+                                                            $sender->sendMessage(C::GREEN . "Rank set ree");
+                                                            break;
+                                                        case "days":
+                                                            $time = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s")." +$length days"));
+                                                            $stmt = $this->db->prepare("INSERT OR REPLACE INTO ranks (player, oldrank, endtime) VALUES (:player, :oldrank, :endtime);");
+                                                            $stmt->bindValue(":player", $playername);
+                                                            $stmt->bindValue(":oldrank", $oldrank);
+                                                            $stmt->bindValue(":endtime", $time);
+                                                            $stmt->execute();
+                                                            $ppplayer = $pp->getPlayer($args[1]);
+                                                            $pp->setGroup($ppplayer, $group);
+                                                            $sender->sendMessage(C::GREEN . "Rank set ree");
+                                                            break;
+                                                    }
+                                                }
+                                            } else {
+                                                $sender->sendMessage(C::GREEN . "$args[3] is not int");
+                                            }
+                                        }
+                                    } else {
+                                        $sender->sendMessage(C::RED . "Group $args[2] not found!");
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return true;
+                }
+                return true;
                 break;
         }
     }
 
-	public function onDisable() : void{
+    public function getTimeLeft($playername) {
+        $date = date("Y-m-d H:i:s");
+        $enddate = $this->getExpiryDate($playername);
+        if($enddate === null) {
+            return null;
+        }
+        if(strtotime($enddate) < time()) {
+            return null;
+        }
+        $datetime1 = date_create($date);
+        $datetime2 = date_create($enddate);
+        $interval = date_diff($datetime1, $datetime2);
+        $min = $interval->format('%i');
+        $sec = $interval->format('%s');
+        $hour = $interval->format('%h');
+        $mon = $interval->format('%m');
+        $day = $interval->format('%d');
+        $year = $interval->format('%y');
+        if ($interval->format('%i%h%d%m%y') == "00000") {
+            return $sec . " Seconds";
 
-	}
+        } else if ($interval->format('%h%d%m%y') == "0000") {
+            return $min . " Minutes";
+        } else if ($interval->format('%d%m%y') == "000") {
+            return $hour . " Hours";
+        } else if ($interval->format('%m%y') == "00") {
+            return $day . " Days";
+        } else if ($interval->format('%y') == "0") {
+            return $mon . " Months";
+        } else {
+            return $year . " Years";
+        }
+    }
+
+    public function getExpiryDate($playername){
+        $stmt = $this->db->prepare("SELECT endtime FROM ranks WHERE player = :player;");
+        $stmt->bindValue(":player", $playername);
+        $result = $stmt->execute();
+        $resultArr = $result->fetchArray(SQLITE3_ASSOC);
+        if(empty($resultArr)) {
+            return null;
+        }
+        return (string)$resultArr["endtime"];
+    }
+
+    public function getOldGroup($playername){
+        $stmt = $this->db->prepare("SELECT oldrank FROM ranks WHERE player = :player;");
+        $stmt->bindValue(":player", $playername);
+        $result = $stmt->execute();
+        $resultArr = $result->fetchArray(SQLITE3_ASSOC);
+        if(empty($resultArr)) {
+            return null;
+        }
+        return (string)$resultArr["oldrank"];
+    }
+
+    public function removeRank($playername) {
+        $pp = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+        $ppplayer = $pp->getPlayer($playername);
+        $group = $this->getOldGroup($playername);
+        if($group === null) {
+            $ppgroup = $pp->getDefaultGroup();
+            $pp->setGroup($ppplayer, $ppgroup);
+            return;
+        }
+        $ppgroup = $pp->getGroup($group);
+        $pp->setGroup($ppplayer, $ppgroup);
+        $stmt = $this->db->prepare("DELETE FROM ranks WHERE player = :player;");
+        $stmt->bindValue(":player", $playername);
+        $stmt->execute();
+    }
+
 }
